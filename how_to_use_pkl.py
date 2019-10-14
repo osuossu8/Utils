45421fb163ad76ce7384939b968fa566ebff70fa
kaerururu <38504303+osuossu8@users.noreply.github.com>
2019-10-14 14:45:39 +0900

    関数化

--- how_to_use_pkl.py
+++ how_to_use_pkl.py
@@ -1,12 +1,13 @@
 
 import pickle
 
-# 保存
-with open('drive/My Drive/ELMo_allenlp_tutorial/x_train.pkl', mode='wb') as f:
-    pickle.dump(x_train, f)
 
-# ロード
-with open('drive/My Drive/ELMo_allenlp_tutorial/x_train.pkl', mode='rb') as f:
-    x_train = pickle.load(f)
-    
-x_train.head()
+def to_pickle(filename, obj):
+    with open(filename, mode='wb') as f:
+        pickle.dump(obj, f)
+
+def unpickle(filename):
+    with open(filename, mode='rb') as fo:
+        p = pickle.load(fo)
+    return p    
+
