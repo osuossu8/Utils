d48d8d3ae65dd392c66d49aa35d22a2ef8942973
osuossu8 <osuosuossu18@gmail.com>
2021-04-05 19:44:48 +0900

    update

---
+++ access_colab.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for i in `seq 0 12`
+do
+  echo "[$i]" ` date '+%y/%m/%d %H:%M:%S'` "connected."
+  # open 'https://colab.research.google.com/drive/1hSkxEdoldzAH47Xg7DC6M58Pe-HQJ70c#scrollTo=V5plmwb4L_9U'
+  open 'https://colab.research.google.com/drive/1k6b7L6Djn66B2JNZ3vRiVsJ7_AZZ2log#scrollTo=V5plmwb4L_9U'
+  sleep 3900
+done
